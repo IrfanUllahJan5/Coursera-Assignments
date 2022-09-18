239a55abd4863ed374359b7b50954fab1bf2669d
IrfanUllahJan5 <113758926+IrfanUllahJan5@users.noreply.github.com>
2022-09-18 14:03:25 +0500

    Update SpeakHello.js

--- Module4/SpeakHello.js
+++ Module4/SpeakHello.js
@@ -9,7 +9,6 @@
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 
-
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
