fa617e21d1bebecf10cacd83a30933e342d91655
IrfanUllahJan5 <113758926+IrfanUllahJan5@users.noreply.github.com>
2022-09-18 17:55:28 +0500

    Delete HelloWorld.js

--- HelloWorld.js
+++
@@ -1,9 +0,0 @@
-(function(window) {
-	var speakWord = "Hello";
-	var helloSpeaker = function (name) {
-		console.log(speakWord + " " + name);
-	}
-
-	window.helloSpeaker = helloSpeaker;
-
-})(window);
